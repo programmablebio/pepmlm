d11fae9fe7c05708f66895b9b0115c6f9ba7f8db
Tianlai Chen <tc289@dcc-login-02.oit.duke.edu>
2023-11-25 16:09:23 -0500

    fix ppl

--- scripts/generation.py
+++ scripts/generation.py
@@ -1,18 +1,56 @@
 def compute_pseudo_perplexity(model, tokenizer, protein_seq, binder_seq):
     sequence = protein_seq + binder_seq
-    tensor_input = tokenizer.encode(sequence, return_tensors='pt').to(model.device)
+    original_input = tokenizer.encode(sequence, return_tensors='pt').to(model.device)
+    length_of_binder = len(binder_seq)
 
-    # Create a mask for the binder sequence
-    binder_mask = torch.zeros(tensor_input.shape).to(model.device)
-    binder_mask[0, -len(binder_seq)-1:-1] = 1
+    # Prepare a batch with each row having one masked token from the binder sequence
+    masked_inputs = original_input.repeat(length_of_binder, 1)
+    positions_to_mask = torch.arange(-length_of_binder - 1, -1, device=model.device)
 
-    # Mask the binder sequence in the input and create labels
-    masked_input = tensor_input.clone().masked_fill_(binder_mask.bool(), tokenizer.mask_token_id)
-    labels = tensor_input.clone().masked_fill_(~binder_mask.bool(), -100)
+    masked_inputs[torch.arange(length_of_binder), positions_to_mask] = tokenizer.mask_token_id
 
+    # Prepare labels for the masked tokens
+    labels = torch.full_like(masked_inputs, -100)
+    labels[torch.arange(length_of_binder), positions_to_mask] = original_input[0, positions_to_mask]
+
+    # Get model predictions and calculate loss
     with torch.no_grad():
-        loss = model(masked_input, labels=labels).loss
-    return np.exp(loss.item())
+        outputs = model(masked_inputs, labels=labels)
+        loss = outputs.loss
+
+    # Loss is already averaged by the model
+    avg_loss = loss.item()
+    pseudo_perplexity = np.exp(avg_loss)
+    return pseudo_perplexity
+
+# Alternative implementation: Use Loop
+def compute_pseudo_perplexity2(model, tokenizer, protein_seq, binder_seq):
+    sequence = protein_seq + binder_seq
+    tensor_input = tokenizer.encode(sequence, return_tensors='pt').to(model.device)
+    total_loss = 0
+
+    # Loop through each token in the binder sequence
+    for i in range(-len(binder_seq)-1, -1):
+        # Create a copy of the original tensor
+        masked_input = tensor_input.clone()
+
+        # Mask one token at a time
+        masked_input[0, i] = tokenizer.mask_token_id
+        # Create labels
+        labels = torch.full(tensor_input.shape, -100).to(model.device)
+        labels[0, i] = tensor_input[0, i]
+
+        # Get model prediction and loss
+        with torch.no_grad():
+            outputs = model(masked_input, labels=labels)
+            total_loss += outputs.loss.item()
+
+    # Calculate the average loss
+    avg_loss = total_loss / len(binder_seq)
+
+    # Calculate pseudo perplexity
+    pseudo_perplexity = np.exp(avg_loss)
+    return pseudo_perplexity
 
 
 def generate_peptide_for_single_sequence(protein_seq, peptide_length = 15, top_k = 3, num_binders = 4):
